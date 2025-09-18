80ae3d3c1fa209df6535c9e54b41fe0e6356e246
maxlarkin <maxlarkin2007@gmail.com>
2025-09-18 20:57:01 +0400

    add download file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 app = FastAPI()
 
@@ -7,6 +7,10 @@ app = FastAPI()
 def root():
     response = '<h1>Hello, world!</h1>'
     return HTMLResponse(content=response)
+
+@app.get('/file', response_class=FileResponse)
+def getFile():
+    return FileResponse('./.gitignore')
 
 
 '''pip unistall -r -f requirements.txt'''
