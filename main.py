ed3bccfe238a1609f25a09f58a34dc185201d6e5
maxlarkin <maxlarkin2007@gmail.com>
2025-09-20 23:14:14 +0400

    add interface of request

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TempRequest(BaseModel):
+    name: str
+    lastname: str
 
 @app.get('/')
 def root():
     return FileResponse('public/index.html')
 
 @app.post('/api/users')
-def userInfo(data = Body(min_length=1)):
-    return {'message': f'user: {data["name"]} {data["lastname"]}'}
+def userInfo(user: TempRequest):
+    return {'message': f'user: {user.name} {user.lastname}'}
 
 '''pip unistall -r -f requirements.txt'''
 '''uvicorn main:app --reload'''
