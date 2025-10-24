ae960b1f99382792483e7450fd8143c1d0ced4f0
maxlarkin <maxlarkin2007@gmail.com>
2025-10-24 22:52:24 +0500

    refactor code pep8

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import uuid
 from fastapi import FastAPI, Body
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+
 
 class User:
     def __init__(self, name, lastname):
@@ -9,19 +10,27 @@ class User:
         self.name = name
         self.lastname = lastname
 
-users = [User('Максим', 'Ларкин'), User('Олег', 'Самойлов'), 
-         User('Олег', 'Михайленков'), User('Сергей', 'Петов'), 
-         User('Кирилл', 'Гулев'), User('Антон', 'Мерин'), 
+
+users = [User('Максим', 'Ларкин'),
+         User('Олег', 'Самойлов'),
+         User('Олег', 'Михайленков'),
+         User('Сергей', 'Петов'),
+         User('Кирилл', 'Гулев'),
+         User('Антон', 'Мерин'),
          User('Форве', 'Эмперрор')]
-#Идет 17 (18) день быссмысленного коммита
+# Идет 18 (19) день быссмысленного коммита
 print(users)
+
+
 def find_user(id: str):
     for user in users:
-        if user['id'] == int(id):
+        if user.id == int(id):
             return user
     return None
 
+
 app = FastAPI()
+
 
 # app.mount('/', StaticFiles(directory='public', html=True))
 @app.get("/")
@@ -33,22 +42,25 @@ def index():
 def get_users():
     return users
 
+
 @app.get('/api/users/{id}')
 def get_user(id: str):
     res = find_user(id)
-    return res if res != None else 'User not found'
+    return res if res is not None else 'User not found'
+
 
 @app.put('/api/users')
-def update_user(data = Body()):
+def update_user(data=Body()):
     user = find_user(data['id'])
-    if user != None:
+    if user is not None:
         user['name'] = data['name'] 
         user['lastname'] = data['lastname']   
         return user
     return 'User not found'
 
+
 @app.post('/api/users')
-def create_user(data = Body()):
+def create_user(data=Body()):
     user = User(data['name'], data['lastname'])
     users.append(user)
     return user
